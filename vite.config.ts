bf7a69a2600e4c889e14930e18b9aa187e02aca6
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2025-08-14 00:10:24 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), tsconfigPaths()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'lib/main.ts'),
+			name: 'mistral-ui',
+			fileName: 'mistral-ui',
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom', 'react/jsx-runtime'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+					'react/jsx-runtime': 'react/jsx-runtime',
+				},
+			},
+		},
+	},
+});
