e17b7a151193b29bd7752faefcb031f71a414f0c
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2025-08-24 17:43:09 -0300

    chore: setup

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+	content: ['./lib/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname =
+	typeof __dirname !== 'undefined'
+		? __dirname
+		: path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	build: {
 		lib: {
-			entry: resolve(__dirname, 'lib/main.ts'),
+			entry: resolve(__dirname, 'lib/index.ts'),
 			name: 'mistral-ui',
 			fileName: 'mistral-ui',
 		},
@@ -22,5 +32,33 @@ export default defineConfig({
 				},
 			},
 		},
+	},
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+					storybookTest({
+						configDir: path.join(dirname, '.storybook'),
+					}),
+				],
+				test: {
+					name: 'storybook',
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: 'playwright',
+						instances: [
+							{
+								browser: 'chromium',
+							},
+						],
+					},
+					setupFiles: ['.storybook/vitest.setup.ts'],
+				},
+			},
+		],
 	},
 });
